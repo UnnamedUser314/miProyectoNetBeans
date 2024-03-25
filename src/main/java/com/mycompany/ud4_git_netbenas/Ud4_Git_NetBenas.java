bbf22c1a112920106fa7aa2353cd7beabbc94851
Kostya <kostya.klevets@gmail.com>
2024-03-25 16:23:46 +0100

    Primer commit proyecto NetBeans

---
+++ src/main/java/com/mycompany/ud4_git_netbenas/Ud4_Git_NetBenas.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ud4_git_netbenas;
+
+/**
+ *
+ * @author Vespertino
+ */
+public class Ud4_Git_NetBenas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+    }
+}
