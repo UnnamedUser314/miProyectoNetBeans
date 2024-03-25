d92086923e46c3930c1e0eb54b0acbc2ad81df1f
Kostya <kostya.klevets@gmail.com>
2024-03-25 16:26:41 +0100

    Añadimos otro printline en el codigo

--- src/main/java/com/mycompany/ud4_git_netbenas/Ud4_Git_NetBenas.java
+++ src/main/java/com/mycompany/ud4_git_netbenas/Ud4_Git_NetBenas.java
@@ -12,6 +12,6 @@ public class Ud4_Git_NetBenas {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        
+        System.out.println("Di nahui");
     }
 }
